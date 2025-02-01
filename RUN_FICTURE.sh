c04f8ac78525f9464feeda1f5fee071fc21d6c8f
seongju-cho <80207883+seongju-cho@users.noreply.github.com>
2025-02-01 18:38:25 +0900

    Update RUN_FICTURE.sh
    add spatula to recover FICTURE output to coordinates

--- RUN_FICTURE.sh
+++ RUN_FICTURE.sh
@@ -4,17 +4,16 @@ set -u
 set -o pipefail
 
 # conda activate FICTURE
-# requirements : 
-# 1. jq / conda install conda-forge::jq
-# 2. FICTURE / pip install ficture
-# 3. Tissue debris or Systemic error region information (polygon) / If you don't see any tissue debris or systemic error, please set NULL
+# requirements : jq, bgzip, tabix, spatula
 
 ### Set Parameters
 
-spaceRanger_output=/home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/1.preprocessing/Lung_Normal_HN00227429/outs
-sample_name=Lung_Normal
-polygon_path=NULL
-outpath=/home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/outs_normal_2
+spaceRanger_output=/home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/1.preprocessing/Lung_tumor_HN00227429/outs
+sample_name=Lung_Tumor
+polygon_path=/home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scripts_runTogether/Tumor_polygon.Rdata
+outpath=/home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/outs_tumor
+spatula_path=/home/sjcho/yard/apps/spatula/spatula/bin/spatula
+scale_factor=4.620840292370345 # change pixel to micrometer in original barcode file
 
 ## FICTURE parameter
 train_width=12 # size of anchor
@@ -28,6 +27,7 @@ tabix_path=/home/sjcho/.conda/envs/FICTURE/bin/tabix
 mkdir -p ${outpath}/1.blacklist
 mkdir -p ${outpath}/2.FormatChange
 mkdir -p ${outpath}/3.FICTURE/${sample_name}
+mkdir -p ${outpath}/5.spatula/${sample_name}
 
 # 1. Build a blacklist
 /home/sjcho/.conda/envs/visiumHD/bin/Rscript /home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scripts_runTogether/1.build_blacklist.R ${spaceRanger_output} ${sample_name} ${polygon_path} ${outpath}/1.blacklist
@@ -37,3 +37,9 @@ bash /home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scri
 
 # 3. Run FICTURE
 bash /home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scripts_runTogether/3.run_FICTURE.sh ${spaceRanger_output} ${outpath}/2.FormatChange ${train_width} ${n_factor} ${n_jobs} ${threads} ${bgzip_path} ${tabix_path} ${outpath}/3.FICTURE/${sample_name}
+
+# 4. sort FICTURE output by one axis / it has no separate storage path
+bash /home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scripts_runTogether/4.sort_ficture.sh ${outpath}/3.FICTURE/${sample_name} ${n_factor} ${train_width}
+
+# 5. recover original coordinates information from FICTURE output by spatula
+bash /home/sjcho/projects/ATLAS_visiumHD/lung_HN00227429_20241003/4.FICTURE/scripts_runTogether/5.spatula.sh ${outpath} ${sample_name} ${n_factor} ${train_width} ${spatula_path} ${scale_factor}
